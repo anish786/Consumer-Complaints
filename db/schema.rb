ef9fa98b992416aec32ee320164d2dd27db1053d
Anish Ali <ali.anish91@gmail.com>
2017-07-24 00:36:00 -0400

    Removed models

--- db/schema.rb
+++ db/schema.rb
@@ -12,25 +12,4 @@
 
 ActiveRecord::Schema.define(version: 20170723011416) do
 
-  create_table "complaints", force: :cascade do |t|
-    t.string   "company"
-    t.string   "company_public_response"
-    t.string   "company_response"
-    t.string   "complaint_id"
-    t.string   "consumer_consent_provided"
-    t.string   "consumer_disputed"
-    t.string   "date_received"
-    t.string   "date_sent_to_company"
-    t.string   "issue"
-    t.string   "product"
-    t.string   "state"
-    t.string   "sub_issue"
-    t.string   "sub_product"
-    t.string   "submitted_via"
-    t.string   "timely"
-    t.string   "zip_code"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-  end
-
 end
